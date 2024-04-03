8cdb82d7b4400df590803fb0c18d9a8fb67fbe39
SOHA44 <sohasayedrefaei@gmail.com>
2024-04-03 20:01:45 +0200

    basic calculator functionality

--- ConsoleApplication29.cpp
+++ ConsoleApplication29.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 using namespace std;
+
+void setGameScore() {
+
+}
+
+void updateGameScore() {
+		
+}
+
 int main() {
+	int num1; int num2;
+	char oprator;
+	cout << "enter 2 numbers " << endl;
+	cin >> num1 >> num2;
+	cout << "enter operator ";
+	cin >> oprator;
+	switch (oprator) {
+		case'+':
+			cout << "the sum =" << num1 + num2;
+			break;
+		case'-':
+			cout << "the sub = " << num1 - num2;
+			break;
+		case '*':
+			cout << "tne product =" << num1 * num2;
+			break;
+		case'/':
+			cout << "the quatient=" << num1 / num2;
+			break;
+	}
+
 	return 0;
 }
